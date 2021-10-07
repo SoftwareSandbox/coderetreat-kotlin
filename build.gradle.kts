64703d1169645df37b8e663ed0f1cbd069525daa
Tim Schraepen <tim.schraepen@sfpd.fgov.be>
2021-10-07 09:50:58 +0200

    Replaced Gradle with Maven so the repo is usable at SFPD

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("kotlin-conventions")
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
-    testImplementation("org.assertj:assertj-core:3.14.0")
-    testImplementation("io.mockk:mockk:1.9.3")
-}
--- src/test/kotlin/be/swsb/coderetreat/HelloTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/HelloTest.kt
@@ -1,9 +1,11 @@
 package be.swsb.coderetreat
 
-import io.mockk.every
-import io.mockk.mockk
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 
 
 class HelloTest {
@@ -21,8 +23,8 @@ class HelloTest {
 
     @Test
     fun `sayGreeting - with mock should return a mocked greeting`() {
-        val helloMock = mockk<HelloService>()
-        every { helloMock.sayGreeting() } returns "Snarf!"
+        val helloMock = mock<HelloService>()
+        whenever(helloMock.sayGreeting()) doReturn("Snarf!")
         assertThat(helloMock.sayGreeting()).isEqualTo("Snarf!")
     }
 }
