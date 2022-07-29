932a2c2423c31c3947f7ce85638389fe34a24f9a
sch3lp <sch3lp@gmail.com>
2022-07-29 21:50:50 +0200

    Visualizing a playing field with all ships sunk shows a motivational message

--- src/test/kotlin/be/swsb/coderetreat/BattleshipTest.kt
+++ src/test/kotlin/be/swsb/coderetreat/BattleshipTest.kt
@@ -29,7 +29,7 @@ class BattleshipTest {
         }
 
         @Test
-        internal fun `a playing field with a horizontal carrier in the top left`() {
+        fun `a playing field with a horizontal carrier in the top left`() {
             val expected = """
                 🛬🛬🛬🛬🛬🟦🟦🟦🟦🟦
                 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦
@@ -48,7 +48,7 @@ class BattleshipTest {
         }
 
         @Test
-        internal fun `a playing field with a vertical carrier in the top left`() {
+        fun `a playing field with a vertical carrier in the top left`() {
             val expected = """
                 🛬🟦🟦🟦🟦🟦🟦🟦🟦🟦
                 🛬🟦🟦🟦🟦🟦🟦🟦🟦🟦
@@ -67,7 +67,7 @@ class BattleshipTest {
         }
 
         @Test
-        internal fun `a playing field with a combination of all the ships`() {
+        fun `a playing field with a combination of all the ships`() {
             val expected = """
                 🛬🟦🟦🟦🟦🟦🟦🟦🟦🟦
                 🛬🟦🟦🟦🚤🚤🟦🟦🟦🟦
@@ -86,11 +86,21 @@ class BattleshipTest {
             val destroyer = Destroyer(At(7, 6), Direction.Vertical) // ⛴
             val submarine = Submarine(At(7, 5), Direction.Horizontal) // 🫧
             val patrolBoat = PatrolBoat(At(4, 1), Direction.Horizontal) //🚤
-            assertThat(PlayingField(setOf(carrier, battleship, destroyer, submarine, patrolBoat)).visualize()).isEqualTo(expected)
+            assertThat(
+                PlayingField(
+                    setOf(
+                        carrier,
+                        battleship,
+                        destroyer,
+                        submarine,
+                        patrolBoat
+                    )
+                ).visualize()
+            ).isEqualTo(expected)
         }
 
         @Test
-        internal fun `a playing field with a ship that was hit`() {
+        fun `a playing field with a ship that was hit`() {
             val expected = """
                 🛬🟦🟦🟦🟦🟦🟦🟦🟦🟦
                 🛬🟦🟦🟦🚤🚤🟦🟦🟦🟦
@@ -115,7 +125,7 @@ class BattleshipTest {
         }
 
         @Test
-        internal fun `a playing field with all ships sunk`() {
+        fun `a playing field with all ships sunk`() {
             val expected = """
                 💥🟦🟦🟦🟦🟦🟦🟦🟦🟦
                 💥🟦🟦🟦💥💥🟦🟦🟦🟦
@@ -127,6 +137,7 @@ class BattleshipTest {
                 🟦🟦💥🟦🟦🟦🟦💥🟦🟦
                 🟦🟦💥🟦🟦🟦🟦💥🟦🟦
                 🟦🟦💥🟦🟦🟦🟦🟦🟦🟦
+                == YOU LOSE 😭 ==
             """.trimIndent()
 
             val carrier = Carrier(At(0, 0), Direction.Vertical) // 🛬
@@ -135,7 +146,7 @@ class BattleshipTest {
             val submarine = Submarine(At(7, 5), Direction.Horizontal) // 🫧
             val patrolBoat = PatrolBoat(At(4, 1), Direction.Horizontal) //🚤
             val playingField = PlayingField(setOf(carrier, battleship, destroyer, submarine, patrolBoat))
-            with(playingField){
+            with(playingField) {
                 fire("A1")
                 fire("A2")
                 fire("A3")
@@ -157,7 +168,6 @@ class BattleshipTest {
                 fire("C8")
                 fire("C9")
                 fire("C10")
-
             }
             assertThat(playingField.visualize()).isEqualTo(expected)
         }
@@ -165,34 +175,40 @@ class BattleshipTest {
 }
 
 fun PlayingField.visualize(): String {
-    return (0..9).joinToString("\n") { row ->
+    val visualizedPlayingField = (0..9).joinToString("\n") { row ->
         (0..9).joinToString("") { column ->
             val shipAt = shipAt(column, row)
             val square = shipAt?.visualize(column, row) ?: Square.Empty
             square.icon
         }
     }
+    return if (allShipsSunk) visualizedPlayingField + "\n== YOU LOSE \uD83D\uDE2D =="
+    else visualizedPlayingField
 }
 
-fun Ship.visualize(x: Int, y: Int)  = if (isDamaged(At(x,y))) Square.Damage else Square.ShipSquare(icon)
+fun Ship.visualize(x: Int, y: Int) = if (isDamaged(At(x, y))) Square.Damage else Square.ShipSquare(icon)
 
 sealed class Square(val icon: String) {
-    object Empty: Square("🟦")
-    object Damage: Square("💥")
+    object Empty : Square("🟦")
+    object Damage : Square("💥")
     class ShipSquare(icon: String) : Square(icon)
 }
 
 class PlayingField(private val ships: Set<Ship> = emptySet()) {
-    private val positions : Map<At, Ship>
-     get() = ships.flatMap { ship -> ship.positions.associateWith { ship }.toList() }.toMap()
-    fun shipAt(x: Int, y: Int): Ship? = shipAt(At(x,y))
-    private fun shipAt(at:At): Ship? = positions[at]
+    val allShipsSunk
+        get() = !ships.isEmpty() && ships.all(Ship::isSunk)
+
+    private val positions: Map<At, Ship>
+        get() = ships.flatMap { ship -> ship.positions.associateWith { ship }.toList() }.toMap()
+
+    fun shipAt(x: Int, y: Int): Ship? = shipAt(At(x, y))
+    private fun shipAt(at: At): Ship? = positions[at]
     fun fire(target: String) {
         val at = At(target)
         shipAt(at)?.damage(at)
     }
-
 }
+
 sealed class Ship(val icon: String, private val size: Int, private val at: At, private val direction: Direction) {
     private val damages = mutableListOf<At>()
 
@@ -200,37 +216,41 @@ sealed class Ship(val icon: String, private val size: Int, private val at: At, p
         damages += at
     }
 
-    fun isDamaged(at: At): Boolean = at in damages
+    fun isDamaged(at: At) = at in damages
+    fun isSunk() = damages.size == size
 
     val positions
-        get() = when(direction) {
-            Direction.Horizontal -> at..At(at.x+(size-1), at.y)
-            Direction.Vertical -> at..At(at.x, at.y+(size-1))
+        get() = when (direction) {
+            Direction.Horizontal -> at..At(at.x + (size - 1), at.y)
+            Direction.Vertical -> at..At(at.x, at.y + (size - 1))
         }
 }
+
 class Carrier(at: At, direction: Direction) : Ship("🛬", 5, at, direction)
 class Battleship(at: At, direction: Direction) : Ship("🛳", 4, at, direction)
 class Destroyer(at: At, direction: Direction) : Ship("⛴", 3, at, direction)
 class Submarine(at: At, direction: Direction) : Ship("🫧", 3, at, direction)
 class PatrolBoat(at: At, direction: Direction) : Ship("🚤", 2, at, direction)
 
-data class At(val x: Int, val y: Int){
+data class At(val x: Int, val y: Int) {
     operator fun rangeTo(other: At): List<At> {
         return when {
-            this.y == other.y -> (this.x..other.x).map { At(it,this.y) }
-            this.x == other.x -> (this.y..other.y).map { At(this.x,it) }
+            this.y == other.y -> (this.x..other.x).map { At(it, this.y) }
+            this.x == other.x -> (this.y..other.y).map { At(this.x, it) }
             else -> emptyList()
         }
     }
+
     companion object {
-        operator fun invoke(string: String) : At {
+        operator fun invoke(string: String): At {
             val col = string.first()
             val row = string.drop(1)
             val x = ('A'..'J').zip(0..9).toMap()[col] ?: error("Column $col out of bounds")
-            return At(x, row.toInt()-1)
+            return At(x, row.toInt() - 1)
         }
     }
 }
+
 enum class Direction {
     Horizontal,
     Vertical
