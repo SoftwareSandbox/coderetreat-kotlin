764560f3803446d8a4cd99110682a6a37379cf8e
sch3lp <sch3lp@gmail.com>
2023-05-05 15:26:05 +0200

    Re-added gradle

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("kotlin-conventions")
+}
+
+dependencies {
+    implementation("dev.forkhandles:result4k:2.2.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.0")
+    testImplementation("org.assertj:assertj-core:3.20.2")
+}
+repositories {
+    mavenCentral()
+}
