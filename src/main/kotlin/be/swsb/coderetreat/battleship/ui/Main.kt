d0f7db9cb75a9b4e818666cf7bdce175bef346da
Sch3lp <sch3lp@gmail.com>
2023-11-18 12:50:29 +0100

    Rendering a grid with Compose

--- src/main/kotlin/be/swsb/coderetreat/battleship/ui/Main.kt
+++ src/main/kotlin/be/swsb/coderetreat/battleship/ui/Main.kt
@@ -1,11 +1,14 @@
 package be.swsb.coderetreat.battleship.ui
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -14,6 +17,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import be.swsb.coderetreat.battleship.logic.EventStream
+import be.swsb.coderetreat.battleship.logic.Game
+import be.swsb.coderetreat.battleship.logic.Piece.*
+import be.swsb.coderetreat.battleship.logic.at
 
 fun main() = application {
     Window(
@@ -21,21 +28,35 @@ fun main() = application {
         title = "Battleship",
         state = rememberWindowState(width = 800.dp, height = 600.dp)
     ) {
-        val count = remember { mutableStateOf(0) }
-        MaterialTheme {
+        MainContent()
+    }
+}
 
-            Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-                Button(modifier = Modifier.align(CenterHorizontally),
-                    onClick = {
-                        count.value++
-                    }) {
-                    Text(if (count.value == 0) "Hello World" else "Clicked ${count.value}!")
-                }
-                Button(modifier = Modifier.align(CenterHorizontally),
-                    onClick = {
-                        count.value = 0
-                    }) {
-                    Text("Reset")
+@Composable
+@Preview
+fun MainContent() {
+    val eventStream = EventStream()
+    val game = Game.startNewGame(eventStream)
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(1.dp)
+        ) {
+            (1..10).map { y ->
+                Row(horizontalArrangement = Arrangement.spacedBy(1.dp)) {
+                    (1..10).map { x ->
+                        val displayText = when (game.piece(at(x, y))) {
+                            Hit -> """💥"""
+                            Sunk -> """🏊"""
+                            CarrierPart -> """🛳"""
+                            BattleshipPart -> """⛴️"""
+                            DestroyerPart -> """🚢"""
+                            SubmarinePart -> """🤿"""
+                            PatrolBoatPart -> """🛥"""
+                            null -> """🌊"""
+                        }
+                        Text(text = displayText)
+                    }
                 }
             }
         }
