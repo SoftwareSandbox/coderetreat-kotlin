edf6625b738458432fc0fb001c42492e479f4dff
Sch3lp <sch3lp@gmail.com>
2026-05-09 11:11:35 +0200

    Bumped kotlin, junit, assertj and gradlew versions

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -11,16 +12,18 @@ repositories {
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.7.3"))
+    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.10.2"))
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.test {
@@ -28,8 +31,8 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        languageVersion = "1.9"
-        freeCompilerArgs = listOf("-Xjsr305=strict")
+    compilerOptions {
+        languageVersion.set(KotlinVersion.KOTLIN_1_9)
+        freeCompilerArgs.set(listOf("-Xjsr305=strict"))
     }
 }
